009d581647565d049f754525922de3476e134e44
ahsan dani <ahsandanyal97@gmail.com>
2022-07-25 21:20:10 +0500

    Updates

---
+++ precache-manifest.aa51a6ebcb20f8a5d272c8a2c71f38fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5995593505f169a723a1",
+    "url": "/static/css/main.a48b8bbd.chunk.css"
+  },
+  {
+    "revision": "5995593505f169a723a1",
+    "url": "/static/js/main.9faf1bf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8783b8ae0ca988025217",
+    "url": "/static/js/2.dfab15d6.chunk.js"
+  },
+  {
+    "revision": "d3f4ef511a230f4b67d4b2ea3ee30b06",
+    "url": "/index.html"
+  }
+];
